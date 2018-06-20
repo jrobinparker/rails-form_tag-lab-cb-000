70f7701d1253ed4f2c108c67894f0a6820c4d50c
Jeremy Parker <jrobinparker@gmail.com>
2018-06-20 03:59:04 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   resources :students, only: [:index, :new, :create]
+=======
+  resources :students
+>>>>>>> 7c7455641fcccbfd23b23d03e7b4776e1c8c0cd6
   get '/students/:id', to: 'students#show', as: 'student'
 end
